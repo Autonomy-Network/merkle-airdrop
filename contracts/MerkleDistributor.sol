6bb89d3b2e0b22d50cd68ba48f3c79953a67d191
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2022-03-19 13:19:30 +0100

    update distributor contract

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -6,36 +6,48 @@ import "./NftContract.sol";
 
 /**
   Ref: https://github.com/Uniswap/merkle-distributor
- */
+*/
 contract MerkleDistributor {
     bytes32 public immutable merkleRoot;
     address public immutable Nft;
+    uint256 private tokenID;
 
+    mapping(address => bool) public claimCheck;
     event Claimed(address account, uint256 tokenID, uint256 amount);
 
     constructor(bytes32 merkleRoot_, address Nft_) {
         merkleRoot = merkleRoot_;
         Nft = Nft_;
+        tokenID = 0;
     }
 
     function claim(
         address account,
-        uint256 tokenID,
         uint256 amount,
+        uint256 tokenID,
         bytes memory data,
         bytes32[] calldata merkleProof
     ) public {
         // Verify the merkle proof.
-        bytes32 node = keccak256(abi.encodePacked(account, tokenID));
+        bytes32 node = keccak256(abi.encodePacked(account));
 
         require(
             MerkleProof.verify(merkleProof, merkleRoot, node),
             "MerkleDistributor: Invalid proof."
         );
 
-        // do your logic accordingly here, call mint function on nft contract (only merkle distributor contract should be able to mint)
-        NftContract(Nft).safeMint(account, tokenID, amount, data );
+        require(
+            claimCheck[account] != true,
+            "MerkleDistributor: account already minted"
+        );
 
-        emit Claimed(account, tokenID, amount );
+        // do your logic accordingly here, call mint function on nft contract (only merkle distributor contract should be able to mint)
+        claimCheck[account] = true;
+        NftContract(Nft).safeMint(account, tokenID, amount, data);
+
+        tokenID++;
+        emit Claimed(account, tokenID, amount);
     }
 }
+
+
